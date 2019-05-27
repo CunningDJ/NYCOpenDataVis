4cd0e4268b49ceb8a3dbae876f20989c74aefcf5
David Cunningham <david7789@gmail.com>
2019-05-27 19:07:43 -0400

    Added Helmet section for HomePage.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 
 import NycComplaintsTable from '../parts/NycComplaintsTable/NycComplaintsTable';
+
+import * as mu from '../metaUtils';
+
+// meta
+const DEFAULT_META = {
+    title: "NYC Complaints Data",
+    description: "NYC Complaints Data from NYC OpenData.",
+}
 
 export interface IHomePageProps {}
 
 const HomePage = (props: IHomePageProps) => (
     <div>
+        <Helmet>
+            {mu.metaTitleTags(DEFAULT_META.title)}
+            {mu.metaDescriptionTags(DEFAULT_META.description)}
+            {mu.metaUrlTags()}
+        </Helmet>
+
         <h1>NYC Complaints Data</h1>
         <NycComplaintsTable />
     </div>
